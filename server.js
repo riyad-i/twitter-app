6d7ea43482b190fd783a789050caf1d00dce4d8b
Riyad <Riyad9876@gmail.com>
2023-08-31 13:03:41 -0400

    middleware, post method

--- server.js
+++ server.js
@@ -3,14 +3,36 @@ const connectDB = require('./utils/connectDB')
 
 const express = require('express')
 
+const Tweet = require('./models/Tweet')
+
 const app = express()
 const PORT = 3000
 
 
+//middleware
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
     res.send('Working!')
+})
+
+
+app.get('/tweets', async (req, res)=> {
+    try {
+        const tweets = await Tweet.find({})
+        res.send(tweets)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+app.post('/tweets', async (req, res) => {
+    const createdTweet = await Tweet.create(req.body)
+    console.log(createdTweet);
+    res.send(createdTweet)
 })
 
 
