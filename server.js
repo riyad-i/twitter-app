b0a6de820e39a9d899b4d7de719004268563572b
Riyad <Riyad9876@gmail.com>
2023-09-07 10:29:55 -0400

    updated node engine in package.json, process.env.PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const Tweet = require('./models/Tweet')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const manyTweets = require('./models/manytweets')
 
